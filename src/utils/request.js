0b7ea3e3488b28a179faa61f9809e0c62e6a6600
root705829 <3525088417@qq.com>
2019-09-04 23:18:22 +0800

    axios和json-bigint

---
+++ src/utils/request.js
@@ -0,0 +1,33 @@
+import axios from 'axios'
+import JSONbig from 'json-bigint'
+
+// 创建个axios实例 用实例发送请求
+const instance = axios.create({
+  timeout: 3000,
+  baseURL: 'http://ttapi.research.itcast.cn'
+})
+
+// transformResponse是在响应拦截器前调用
+instance.defaults.transformResponse = [function (result) {
+  try {
+    //   数据可能不是标准JSON，因此会导致JSONbig.parse(result) 转换失败 报错
+    return JSONbig.parse(result)
+  } catch (err) {
+    //   转换失败的数据直接原样返回
+    return result
+  }
+}]
+
+instance.interceptors.request.use(function (config) {
+  return config
+}, function (err) {
+  return Promise.reject(err)
+})
+
+instance.interceptors.response.use(function (response) {
+  return response.data.data ? response.data.data : response.data
+}, function (err) {
+  return Promise.reject(err)
+})
+
+export default instance
